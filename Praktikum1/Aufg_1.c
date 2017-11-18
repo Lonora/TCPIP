569dbc68e43568563971a4e16a7957ef21e54b72
Lonora <loua25n_raphael@yahoo.fr>
2017-11-18 01:00:16 +0100

    Update Aufg_1.c

--- Praktikum1/Aufg_1.c
+++ Praktikum1/Aufg_1.c
@@ -4,16 +4,16 @@
 #include <sys/wait.h>
 //#include "c3includes.h"
 int main() {
-  pid_t  pid = getpid();
-  printf ("Meine PID = %d\n", pid);
+  	pid_t  pid = getpid();
+  	printf ("Meine PID = %d\n", pid);
 
-  pid = getppid();
-  printf ("Meine Eltern-PPID = %d\n", pid);
+  	pid = getppid();
+  	printf ("Meine Eltern-PPID = %d\n", pid);
 
-  uid_t uid = getuid();
-  printf ("Meine User-UID = %d\n", uid);
+  	uid_t uid = getuid();
+  	printf ("Meine User-UID = %d\n", uid);
 
-  gid_t gid = getgid();
-  printf ("Meine Group-GID = %d\n", gid);
+  	gid_t gid = getgid();
+  	printf ("Meine Group-GID = %d\n", gid);
 	return 0;
 }
